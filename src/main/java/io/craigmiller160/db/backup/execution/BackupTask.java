63bd1578a93905007ae5028328e7aa4e7b8271ab
Craig Miller <craigmiller160@gmail.com>
2020-11-24 17:58:46 -0500

    Setting up the running of the command

--- src/main/java/io/craigmiller160/db/backup/execution/BackupTask.java
+++ src/main/java/io/craigmiller160/db/backup/execution/BackupTask.java
@@ -4,6 +4,8 @@ import io.craigmiller160.db.backup.properties.PropertyStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Map;
+
 public class BackupTask implements Runnable {
 
     private static final Logger log = LoggerFactory.getLogger(BackupTask.class);
@@ -32,6 +34,21 @@ public class BackupTask implements Runnable {
     @Override
     public void run() {
         log.info("Running backup for Database {} and Schema {}", database, schema);
+
+        final var command = new String[] {
+                "pg_dump",
+                database,
+                "-n",
+                schema,
+                "-h",
+                propStore.getPostgresHost(),
+                "-p",
+                propStore.getPostgresPort(),
+                "-U",
+                propStore.getPostgresUser()
+        };
+        final var environment = Map.of("PGPASSWORD", propStore.getPostgresPassword());
+        final var process = processProvider.provide(command, environment);
     }
 
 }
