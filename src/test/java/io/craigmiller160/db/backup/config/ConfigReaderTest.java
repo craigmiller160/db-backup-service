b73499816366ac6e25aa5f9092a0045f04023881
Craig Miller <craigmiller160@gmail.com>
2020-11-24 15:30:24 -0500

    Stubbed out unit test

---
+++ src/test/java/io/craigmiller160/db/backup/config/ConfigReaderTest.java
@@ -0,0 +1,12 @@
+package io.craigmiller160.db.backup.config;
+
+import org.junit.jupiter.api.Test;
+
+public class ConfigReaderTest {
+
+    @Test
+    public void test_readBackupConfig() throws Exception {
+        throw new Exception();
+    }
+
+}
