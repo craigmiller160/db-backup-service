08a2aa05c161bc8d99d0b2bfa68d5bd9b8632f1a
Craig Miller <craigmiller160@gmail.com>
2020-11-24 15:54:50 -0500

    Added logging dependencies

--- src/main/java/io/craigmiller160/db/backup/Runner.java
+++ src/main/java/io/craigmiller160/db/backup/Runner.java
@@ -1,9 +1,16 @@
 package io.craigmiller160.db.backup;
 
+import io.craigmiller160.db.backup.properties.PropertyReader;
+
 public class Runner {
 
     public static void main(final String[] args) {
-
+        try {
+            final var propReader = new PropertyReader();
+            final var propStore = propReader.readProperties();
+        } catch (final Exception ex) {
+            ex.printStackTrace(); // TODO handle this better
+        }
     }
 
 }
