a87ba0c2c3e41def1a31032aa21fdfc4e5ced621
Craig Miller <craigmiller160@gmail.com>
2020-11-24 15:35:49 -0500

    Implemented test

--- src/test/java/io/craigmiller160/db/backup/config/ConfigReaderTest.java
+++ src/test/java/io/craigmiller160/db/backup/config/ConfigReaderTest.java
@@ -1,12 +1,29 @@
 package io.craigmiller160.db.backup.config;
 
+import io.craigmiller160.db.backup.config.dto.BackupConfig;
+import io.craigmiller160.db.backup.config.dto.DatabaseConfig;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ConfigReaderTest {
 
+    private final ConfigReader configReader = new ConfigReader();
+
     @Test
     public void test_readBackupConfig() throws Exception {
-        throw new Exception();
+        final var expected = new BackupConfig(
+                List.of(
+                        new DatabaseConfig(
+                                "vm_dev",
+                                List.of("public")
+                        )
+                )
+        );
+        final var actual = configReader.readBackupConfig();
+        assertEquals(expected, actual);
     }
 
 }
