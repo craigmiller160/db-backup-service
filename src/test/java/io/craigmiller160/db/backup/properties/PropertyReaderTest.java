f57a6d626bba46f3fef77605bf09019a80ae341b
Craig Miller <craigmiller160@gmail.com>
2020-11-24 15:50:59 -0500

    Wrote tests for PropertyReader

---
+++ src/test/java/io/craigmiller160/db/backup/properties/PropertyReaderTest.java
@@ -0,0 +1,20 @@
+package io.craigmiller160.db.backup.properties;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PropertyReaderTest {
+
+    private final PropertyReader propertyReader = new PropertyReader();
+
+    @Test
+    public void test_readProperties() throws Exception {
+        final var propStore = propertyReader.readProperties();
+        assertEquals("localhost", propStore.getPostgresHost());
+        assertEquals("30001", propStore.getPostgresPort());
+        assertEquals("user", propStore.getPostgresUser());
+        assertEquals("password", propStore.getPostgresPassword());
+    }
+
+}
