9a5ed179d99497939e6cb651b75f789147d8e635
Craig Miller <craigmiller160@gmail.com>
2020-11-24 16:00:24 -0500

    Trying to setup logging

--- src/main/java/io/craigmiller160/db/backup/Runner.java
+++ src/main/java/io/craigmiller160/db/backup/Runner.java
@@ -1,10 +1,15 @@
 package io.craigmiller160.db.backup;
 
 import io.craigmiller160.db.backup.properties.PropertyReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Runner {
 
     public static void main(final String[] args) {
+        final Logger log = LoggerFactory.getLogger("io.craigmiller160.Runner");
+        log.info("Hello World");
+
         try {
             final var propReader = new PropertyReader();
             final var propStore = propReader.readProperties();
