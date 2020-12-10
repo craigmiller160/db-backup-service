cfbfa9736d025547ebf9e3f7e35e827b2484df66
Craig Miller <craigmiller160@gmail.com>
2020-12-10 15:17:16 -0500

    Stubbed out CleanupTask

---
+++ src/main/java/io/craigmiller160/db/backup/execution/CleanupTask.java
@@ -0,0 +1,47 @@
+/*
+ *     db-backup-service
+ *     Copyright (C) 2020 Craig Miller
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package io.craigmiller160.db.backup.execution;
+
+import io.craigmiller160.db.backup.properties.PropertyStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CleanupTask implements Runnable {
+
+    private static final Logger log = LoggerFactory.getLogger(CleanupTask.class);
+
+    private final PropertyStore propStore;
+    private final String database;
+    private final String schema;
+
+    public CleanupTask(final PropertyStore propStore,
+                       final String database,
+                       final String schema) {
+        this.propStore = propStore;
+        this.database = database;
+        this.schema = schema;
+    }
+
+    @Override
+    public void run() {
+        log.info("Running cleanup for Database {} and Schema {}", database, schema);
+        // TODO finish this
+    }
+
+}
