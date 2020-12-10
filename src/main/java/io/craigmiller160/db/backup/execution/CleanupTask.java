b7b77dc7cadd683868491efd601a73d568ef5972
Craig Miller <craigmiller160@gmail.com>
2020-12-10 16:01:11 -0500

    Removed extra import

--- src/main/java/io/craigmiller160/db/backup/execution/CleanupTask.java
+++ src/main/java/io/craigmiller160/db/backup/execution/CleanupTask.java
@@ -25,7 +25,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
