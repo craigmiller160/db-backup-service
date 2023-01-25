9ac88d9410695928a278c4cb6b1bfd419fdf09d5
Craig Miller <craigmiller160@gmail.com>
2023-01-25 18:21:49 -0500

    Setting up dev run config

--- run.sh
+++ run.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-mvn clean compile exec:java \
-  -Ddb.mongo.user=$MONGO_ROOT_USER \
-  -Ddb.mongo.password=$MONGO_ROOT_PASSWORD \
-  -Ddb.postgres.user= \
-  -Ddb.postgres.password=
+mvn clean compile exec:java
