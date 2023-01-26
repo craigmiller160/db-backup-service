d60f4865e64d843a54c424beabdbbd32b0e18d6e
Craig Miller <craigmiller160@gmail.com>
2023-01-25 22:38:16 -0500

    Counts work

--- deploy/terraform/clients.tf
+++ deploy/terraform/clients.tf
@@ -50,40 +50,44 @@ resource "keycloak_role" "db_backup_service_access_role_prod" {
   name = local.access_role_common.name
 }
 
-# TODO finish this and explore ways to run it without email service setup yet
-
 data "keycloak_openid_client" "email_service_client_dev" {
+  count = 0
   realm_id = data.keycloak_realm.apps_dev.id
   client_id = "email-service"
 }
 
 data "keycloak_openid_client" "email_service_client_prod" {
+  count = 0
   realm_id = data.keycloak_realm.apps_prod.id
   client_id = "email-service"
 }
 
 data "keycloak_role" "email_service_access_role_dev" {
+  count = 0
   realm_id = data.keycloak_realm.apps_dev.id
-  client_id = data.keycloak_openid_client.email_service_client_dev.id
+  client_id = data.keycloak_openid_client.email_service_client_dev[0].id
   name = local.access_role_common.name
 }
 
 data "keycloak_role" "email_service_access_role_prod" {
+  count = 0
   realm_id = data.keycloak_realm.apps_prod.id
-  client_id = data.keycloak_openid_client.email_service_client_prod.id
+  client_id = data.keycloak_openid_client.email_service_client_prod[0].id
   name = local.access_role_common.name
 }
 
 resource "keycloak_openid_client_service_account_role" "db_backup_service_email_service_access_dev" {
+  count = 0
   realm_id = data.keycloak_realm.apps_dev.id
   service_account_user_id = keycloak_openid_client.db_backup_service_dev.service_account_user_id
-  client_id = data.keycloak_openid_client.email_service_client_dev.id
+  client_id = data.keycloak_openid_client.email_service_client_dev[0].id
   role = local.access_role_common.name
 }
 
 resource "keycloak_openid_client_service_account_role" "db_backup_service_email_service_access_prod" {
+  count = 0
   realm_id = data.keycloak_realm.apps_prod.id
   service_account_user_id = keycloak_openid_client.db_backup_service_prod.service_account_user_id
-  client_id = data.keycloak_openid_client.email_service_client_prod.id
+  client_id = data.keycloak_openid_client.email_service_client_prod[0].id
   role = local.access_role_common.name
 }
