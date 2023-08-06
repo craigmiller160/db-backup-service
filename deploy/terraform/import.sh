abce4e8d223bfc3f229d70b21fff5d0038fe9d17
Craig Miller <craigmiller160@gmail.com>
2023-08-05 21:19:14 -0400

    Working on import script

--- deploy/terraform/import.sh
+++ deploy/terraform/import.sh
@@ -15,6 +15,12 @@ function plan {
 import "keycloak_openid_client.db_backup_service_dev" "apps-dev/"
 import "keycloak_openid_client.db_backup_service_prod" "apps-prod/"
 
+import "keycloak_role.db_backup_service_access_role_dev" "apps-dev/"
+import "keycloak_role.db_backup_service_access_role_prod" "apps-prod/"
+
+import "keycloak_openid_client_service_account_role.db_backup_service_email_service_access_dev" "apps-dev/"
+import "keycloak_openid_client_service_account_role.db_backup_service_email_service_access_prod" "apps-prod/"
+
 # TODO not done yet
 
 plan
